c762e324a515394dc2e866001647691e1c91112e
kzlf <158624901+clown213213@users.noreply.github.com>
2024-02-05 20:57:11 +0300

    Update kumir.js
    удалил комментарии

--- kumir.js
+++ kumir.js
@@ -32,50 +32,39 @@ document.getElementById('loadButton').addEventListener('click', function() {
 	reader.readAsText(file);
   });
 
-//запуск транспайлера
 kumir.start = function(commands) {
 	
-	//Скрытие текста с кавычками (чтобы он не менялся)
 	let substring=commands.match(/'[^']*'|"[^"]*"/g);
 	for (let i in substring) commands = commands.replace(substring[i],'$_'+i);
 	
-	commands = ' ' + commands.replace(/\n/g,' \n ') + ' '; //волшебный костыль №1
-	commands = commands.replace(/\(/g,' ( ').replace(/\)/g,' ) '); //волшебный костыль №2 (добавление пробелов перед и после скобок)
-	
-	//Проверка на наличие и парсинг команд для исполнителя Робот
+	commands = ' ' + commands.replace(/\n/g,' \n ') + ' ';
+	commands = commands.replace(/\(/g,' ( ').replace(/\)/g,' ) ');
 	robot.tick = 0;
 	if (robot) commands = robot.parseCommand(commands);
 
-	commands = kumir.parseCommand(commands); //трансляция в JavaScript
+	commands = kumir.parseCommand(commands);
 	console.log(commands)
-	for (let i in substring) commands = commands.replace('$_'+i,substring[i]); //Возврат текста в кавычках
+	for (let i in substring) commands = commands.replace('$_'+i,substring[i]);
 
 	console.log(commands)
 
-	/**
-	* Данная конструкция отлавливает ошибки,
-	* основной catch отлавливает ошибки синтаксиса,
-	* catch внутри eval отлавливает остальные ошибки программы
-	*/
 	try {
 		eval('try{'+commands+'}catch(e){if(e=="collision") kumir.error("Столкновение с препятствием!"); else kumir.error("Ошибка в строке "+e.stack.match(/<anonymous>:(\\d+):/)[1]+"!");}');
 	}
 	catch(e) {kumir.error('Ошибка синтаксиса!');}
 }
 
-//Парсинг команд
 kumir.parseCommand = function(commands) {
 
 	let jsCommand ='';
 	
-	//Парсинг основных команд
 	commands.split('\n').forEach(function(command) {
-				if(/\sprocedure\s/.test(command)) command = kumir.parseFunction(command); //замена объявления функции
+				if(/\sprocedure\s/.test(command)) command = kumir.parseFunction(command);
 		command = command.replace(/\sifBlock (.+)/g ,'if (robot.on$1){')
-		.replace(/\selse\s/g,' }else{ ') //замена начала альтернативных команд
-		.replace(/\sendif\s/g,' } ') //замена конца условия
+		.replace(/\selse\s/g,' }else{ ')
+		.replace(/\sendif\s/g,' } ')
 		.replace(/\srepeat (.+)/g,'for(i=1;i<=($1);i++){')
-		.replace(/\sendrepeat\s/g,' } ') //замена конца цикла while или for
+		.replace(/\sendrepeat\s/g,' } ')
 		.replace(/\sendproc\s/g,' }; ')
 		.replace(/\sstartproc\s/g,' { ')	
 		.replace(/\scall\s(.+)/g,' $1();')
@@ -86,14 +75,12 @@ kumir.parseCommand = function(commands) {
 	return jsCommand;
 }
 
-//Парсинг объявления функций
 kumir.parseFunction = function(command) {
 	return command.replace(/procedure\s+$/g,'')
 				.replace(/procedure\s(.+)/g,'function $1 (){')
 				.replace(/\s*(?:log|lit|str|int|float)/g,' ')
 }
 
-//вывод сообщений
 kumir.print = function() {
 	let message = '';
 	for (let i in arguments) message+=arguments[i];
@@ -101,13 +88,11 @@ kumir.print = function() {
 	document.dispatchEvent(event);
 }
 
-//вывод ошибок
 kumir.error = function(errorMsg) {
 	let event = new CustomEvent('error',{detail:errorMsg});
 	document.dispatchEvent(event);
 }
 
-//ввод переменных
 kumir.read = function(args) {
 	let s = prompt('Введите переменные через пробел');
 	s = s.split(' ');
